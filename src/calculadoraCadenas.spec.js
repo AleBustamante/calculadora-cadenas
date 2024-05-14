9c621b32aee4c3657684baa4f580069c1cd4e14b
AleBustamante <adbdc.u@gmail.com>
2024-05-13 23:09:06 -0400

    Refactorizadas pruebas y funcion para usar un caracter enviado por el usuario

--- src/calculadoraCadenas.spec.js
+++ src/calculadoraCadenas.spec.js
@@ -2,21 +2,21 @@ import calcularCadena from "./calculadoraCadenas.js";
 
 describe("Calcular suma de numeros en cadena", () => {
   it("Cuando se pasa una cadena vacia deberia dar como resultado 0", () => {
-    expect(calcularCadena("")).toEqual(0);
+    expect(calcularCadena("", "")).toEqual(0);
   });
   it("Cuando se pasa una cadena con un solo elemento retorna ese mismo elemento", () => {
-    expect(calcularCadena("5")).toEqual(5);
+    expect(calcularCadena("5", "")).toEqual(5);
   });
   it("Cuando se pasa una cadena con dos elementos separados por un espacio devuelve su suma", () => {
-    expect(calcularCadena("5 4")).toEqual(9);
+    expect(calcularCadena("5 4", "")).toEqual(9);
   });
   it("Cuando se pasa una cadena con varios elementos separados por un espacio devuelve su suma", () => {
-    expect(calcularCadena("5 4 23 1 7")).toEqual(40);
+    expect(calcularCadena("5 4 23 1 7", "")).toEqual(40);
   });
   it("Cuando se pasa una cadena con varios elementos separados por espacios o comas devuelve su suma", () => {
-    expect(calcularCadena("5,4 23,1,7")).toEqual(40);
+    expect(calcularCadena("5,4 23,1,7", "")).toEqual(40);
   });
   it("Devuelve la suma para una cadena con elementos separados por guiones, comas o espacios", () => {
-    expect(calcularCadena("5,4 23,1,7-8-10")).toEqual(58);
+    expect(calcularCadena("5,4 23,1,7-8-10", "")).toEqual(58);
   });
 });
